92504594058cb59a433afda8773c554699fff0d9
Epap <peia@inbox.ru>
2023-03-24 15:26:52 +0300

    Stage 3/5: The game is started

--- Indigo Card Game/task/src/indigo/Main.kt
+++ Indigo Card Game/task/src/indigo/Main.kt
@@ -1,5 +1,8 @@
 package indigo
 
+var cardsInHand = mutableListOf<String>()
+var computerCards = mutableListOf<String>()
+var cardsOnTable = mutableListOf<String>()
 val ranks = listOf("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K")
 val suits = listOf("♦", "♥", "♠", "♣")
 val deck = buildList {
@@ -7,53 +10,94 @@ val deck = buildList {
         for (j in ranks) add("$j$i")
     }
 }
+const val TITLE = "Indigo Card Game"
+const val  INIT_TABLE = 4
+const val INIT_HAND = 6
 var currentDeck = deck.toMutableList()
+var play = true
+var userTurn = true
 
 fun main() {
-    menu()
-    println("Bye")
+    play()
 }
 
-fun get() {
-    println("Number of cards:")
-    val number: Int
-    try {
-        number = readln().toInt()
-    } catch (e: Exception) {
-        println("Invalid number of cards.")
+fun play() {
+    println(TITLE)
+    initGame()
+
+    while (play) {
+        turn()
+    }
+
+    println("Game Over")
+}
+
+fun initGame() {
+    playFirst()
+    shuffle()
+    cardsOnTable = get(INIT_TABLE)
+    println("Initial cards on the table: ${cardsOnTable.joinToString(" ")}")
+    cardsInHand = get(INIT_HAND)
+    computerCards = get(INIT_HAND)
+    shuffle()
+}
+
+fun turn() {
+    println("${cardsOnTable.size} cards on the table, and the top card is ${cardsOnTable.last()}")
+    if (cardsOnTable.size == deck.size) {
+        play = false
         return
     }
-    if (number !in 1..deck.size) {
-        println("Invalid number of cards.")
-        return
+    if (userTurn) {
+        if (cardsInHand.isEmpty()) cardsInHand = get(INIT_HAND)
+        print("Cards in hand: ")
+        (0 until cardsInHand.size).forEach {
+            print("${it + 1})${cardsInHand[it]} ")
+        }
+        println()
+        while (true) {
+            try {
+                println("Choose a card to play (1-${cardsInHand.size}):")
+                val s = readln()
+                if (s == "exit") {
+                    play = false
+                    return
+                }
+                val n = s.toInt()
+                cardsOnTable.add(cardsInHand.removeAt(n - 1))
+            } catch (e: Exception) {
+                continue
+            }
+            break
+        }
+    } else {
+        if (computerCards.isEmpty()) computerCards = get(INIT_HAND)
+        cardsOnTable.add(computerCards.removeFirst())
+        println("Computer plays ${cardsOnTable.last()}")
     }
-    if (currentDeck.size < number) {
-        println("The remaining cards are insufficient to meet the request.")
-        return
+    userTurn = !userTurn
+    shuffle()
+}
+
+fun playFirst() {
+    while (true) {
+        println("Play first?")
+        when (readln().lowercase()) {
+            "yes" -> return
+            "no" -> {
+                userTurn = false
+                return
+            }
+        }
     }
-    for (i in 1..number) print("${currentDeck.removeFirst()} ")
-    println()
+}
+
+fun get(number: Int): MutableList<String> {
+    val result = mutableListOf<String>()
+    for (i in 1..number) result.add(currentDeck.removeFirst())
+    return result
 }
 
 fun shuffle() {
     currentDeck.shuffled()
-    println("Card deck is shuffled.")
-}
-
-fun reset() {
-    currentDeck = deck.toMutableList()
-    println("Card deck is reset.")
-}
-
-fun menu() {
-    while (true) {
-        println("Choose an action (reset, shuffle, get, exit):")
-        when (readln()) {
-            "reset" -> reset()
-            "shuffle" -> shuffle()
-            "get" -> get()
-            "exit" -> break
-            else -> println("Wrong action.")
-        }
-    }
 }
